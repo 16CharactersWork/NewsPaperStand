07ab27e52b042decb85e033d826617d864c24c69
16CharactersWork <github.zzxnb@simplelogin.com>
2022-08-29 02:05:27 -0400

    Workdarn it

--- newsp-cli.sh
+++ newsp-cli.sh
@@ -7,6 +7,6 @@ dependencies=("ls" "cat" "curl" "sed" "awk" "tr" "du" "rm" "mkdir" "git" "diff" 
 github_source="https://raw.githubusercontent.com/16CharactersWork/master/newsp-cli"
 
 #Scraping
-re
 
-curl --silent "https://mgreader.com/?cat=231&s=New+York"
+curl "https://mgreader.com/?cat=231&s=New+York+Times" | pup "header h2 a" | cut -d '"' -f2 | pup 'text{}'
+
