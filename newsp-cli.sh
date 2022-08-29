7ca930ad58133ee78d06deb98465c17de7a63ae2
16CharactersWork <github.zzxnb@simplelogin.com>
2022-08-29 12:39:25 -0400

    fun

--- newsp-cli.sh
+++ newsp-cli.sh
@@ -6,22 +6,31 @@ cache_dir="${HOME}/.cache/news-cli"
 dependencies=("ls" "cat" "curl" "sed" "awk" "tr" "du" "rm" "mkdir" "git" "diff" "patch" "zathura" "pup")
 github_source="https://raw.githubusercontent.com/16CharactersWork/master/newsp-cli"
 
-    echo What newspaper are you looking for?
-    read newspaper_input
+count =0
+
+echo What newspaper are you looking for?
+read newspaper_input
 
 
 #Replace spaces with pluses
 newspaper_input="$(printf "%s" "${newspaper}" | tr " " "+")"
 
 #The one command I can get to find everything 
-scapemg=$(curl -s "https://mgreader.com/?cat=231&s=${newspaper}" | 
+for each in $(curl -s "https://mgreader.com/?cat=231&s=${newspaper}" | 
 pup "header h2 a" | 
 cut -d '"' -f2 | 
 pup 'text{}' 
-)
+); do
+  count=$(( count + 1 ))
+  echo "${count}) $each"
+done
+ 
+if [ "$count" -eq 0 ]; then
+  echo "No disks found."
+fi
 
-echo "select your choice, and select only the link above the date"
-echo $scapemg
+#echo "select your choice, and select only the link above the date"
+#select choice in printf$scapemg
 
 #found_link=$(curl -s 'https://mgreader.com/the-new-york-times-25-august-2022.html' | 
 #pup 'a[target="_blank"]' | 
