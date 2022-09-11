81d0f4edc6fe9c7e89b2215b767fe4cb9c659534
16CharactersWork <github.zzxnb@simplelogin.com>
2022-09-10 23:41:13 -0400

    new

--- newsp-cli.py
+++ newsp-cli.py
@@ -1,3 +1,4 @@
+from select import select
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -25,7 +26,7 @@ def urlifyundo(p):
 
 
 options = Options()
-options.headless = True
+options.headless = False
 driver = webdriver.Firefox(options=options, executable_path=GeckoDriverManager().install())
 
 url = "https://mgreader.com/?cat=231&s="
@@ -43,10 +44,14 @@ newspsearch = urlifyundo(newspsearch)
 list_links=driver.find_elements(By.XPATH, "//a[contains(text(),'" + newspsearch + "')]")
 for i in list_links:
     print (i.get_attribute('title'))
-
-
-#Ask user for webpage
+    #Ask user for webpage
+userselection = input("What title would you like to read:")
+userselections = driver.find_element(By.CSS_SELECTOR, "h2[class='entry-title'] a[title='" + userselection + "']")
+linkfound1 = userselections.get_attribute('href')
+driver.get(linkfound1)
+driver.implicitly_wait(2)
+linkfound2 = driver.find_element(By.CSS_SELECTOR, ".btn.btn-dark")
+linkfound2.click
 #dateselected =  urlify(input("Which selection would you like to make?: "))
-
-#printnewspsearch = driver.get
-driver.quit()
+# #printnewspsearch = driver.g
+#driver.quit()
